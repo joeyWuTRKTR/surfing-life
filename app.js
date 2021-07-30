c47ddc8336bbf6c92b22c4a0ed6d52a3ed295092
joeyWuTRKTR <godatascial@yahoo.com>
2021-07-30 09:38:09 +0800

    feat: add youtube video list

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const router = require('./routes/index')
 const app = express()
 
 app.engine('hbs', exphbs({ extname:'.hbs', defaultLayout: 'main' }))
 app.set('view engine', 'hbs')
 
-app.get('/', (req, res) => {
-  res.render('index')
-})
+app.use(express.static('public'))
+app.use(router)
 
 app.listen(3000, () => {
   console.log('App is listening on port 3000.')
---
+++ routes/modules/home.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const router = express.Router()
+// const Surfers = require('../../config/surfers.json')
+
+router.get('/', (req, res) => {
+  console.log('home.js connected!')
+  res.render('index',)
+})
+
+module.exports = router
+
+// { Surfers: Surfers.surfers }
